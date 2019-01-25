e77a000988a212015fa6c218e941393ae0e144d9
ArchibaldChen <chen-sj16@mails.tsinghua.edu.cn>
2019-01-25 11:18:10 +0800

    Add files via upload
    为了完成作业，参考verilog版本写的串行驱动，一个小尝试。

---
+++ lcd_control.vhd
@@ -0,0 +1,178 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity lcd_control is
+port(clk:in std_logic;
+rst:in std_logic;
+read_data:in std_logic_vector(7 downto 0);
+read_add:out std_logic_vector(9 downto 0);
+spi_done:in std_logic;
+spi_start:out std_logic;
+spi_data:out std_logic_vector(9 downto 0);testpin2:out std_logic;clk_2n:out std_logic
+);
+end entity;
+
+architecture behave of lcd_control is
+	--constant f25:std_logic_vector:="1111010000100011111";--用于40Hz的分频
+	--signal c:std_logic_vector(18 downto 0);--这里尝试采用二进制数组代替一般的计数以满足/2的要求
+	--位操作实现过于困难，考虑采用土办法替代
+	--constant f25:integer:=499999;
+	constant f25:integer:=416667;
+	signal c:integer;
+	signal isstart:std_logic_vector(1 downto 0);
+	--signal i:std_logic_vector(5 downto 0);
+	signal i:integer;
+	signal rdata:std_logic_vector(9 downto 0);
+	signal x:std_logic_vector(7 downto 0);
+	signal y:std_logic_vector(3 downto 0);
+	--signal y:bit_vector(3 downto 0);
+	signal isdone,isspi_start:std_logic;
+	signal temp_add:std_logic_vector(11 downto 0);
+	
+	begin
+	process(clk,rst)
+	variable temp:integer;
+	begin
+	if(clk'event and clk='1')then
+	if(rst='0')then 
+	c<=0;
+	elsif(c=f25)then
+	c<=0;
+	else
+	temp:=c;
+	c<=temp+1;
+	end if;
+	end if;
+	end process;
+	
+	process(clk,rst)
+	begin
+	if(clk'event and clk='1')then
+	if(rst='0')then isstart<="10";
+	elsif(c=f25)then isstart<="01";
+	elsif(isdone='1')then isstart<="00";
+	end if;
+	end if;
+	end process;
+	
+	process(clk,rst)
+	--variable te:std_logic_vector(5 downto 0);
+	variable te:integer;
+	variable tex:std_logic_vector(7 downto 0);
+	variable tey:std_logic_vector(3 downto 0);
+	begin
+	if(clk'event and clk='1')then
+	if(rst='0')then
+	--i<="000000";
+	i<=0;
+	rdata<="11"&"00101111";
+	x<="00000000";
+	y<="0000";
+	isspi_start<='0';
+	isdone<='0';
+	elsif(isstart(1)='1')then--初始化功能
+		case i is
+		when 0=>if(spi_done='1')then 
+		isspi_start<='0';te:=i;i<=te+1;
+		else rdata<="00"&"10101111";isspi_start<='1';end if;
+		when 1=>if(spi_done='1')then 
+		isspi_start<='0';te:=i;i<=te+1;
+		else rdata<="00"&"01000000";isspi_start<='1';end if;
+		when 2=>if(spi_done='1')then 
+		isspi_start<='0';te:=i;i<=te+1;
+		else rdata<="00"&"10100000";isspi_start<='1';end if;
+		--else rdata<="00"&"10100110";isspi_start<='1';end if;
+		when 3=>if(spi_done='1')then 
+		isspi_start<='0';te:=i;i<=te+1;
+		--else rdata<="00"&"10100000";isspi_start<='1';end if;
+		else rdata<="00"&"10100110";isspi_start<='1';end if;
+		when 4=>if(spi_done='1')then 
+		isspi_start<='0';te:=i;i<=te+1;
+		--else rdata<="00"&"11001000";isspi_start<='1';end if;
+		else rdata<="00"&"10100100";isspi_start<='1';end if;
+		when 5=>if(spi_done='1')then 
+		isspi_start<='0';te:=i;i<=te+1;
+		else rdata<="00"&"10100010";isspi_start<='1';end if;
+		when 6=>if(spi_done='1')then 
+		isspi_start<='0';te:=i;i<=te+1;
+		else rdata<="00"&"11001000";isspi_start<='1';end if;
+		when 7=>if(spi_done='1')then 
+		isspi_start<='0';te:=i;i<=te+1;
+		else rdata<="00"&"00101111";isspi_start<='1';end if;
+		when 8=>if(spi_done='1')then 
+		isspi_start<='0';te:=i;i<=te+1;
+		else rdata<="00"&"00100100";isspi_start<='1';end if;
+		when 9=>if(spi_done='1')then 
+		isspi_start<='0';te:=i;i<=te+1;
+		else rdata<="00"&"10000001";isspi_start<='1';end if;
+		when 10=>if(spi_done='1')then 
+		isspi_start<='0';te:=i;i<=te+1;
+		else rdata<="00"&"00100100";isspi_start<='1';end if;
+		when 11=>rdata<="11"&"00101111";isdone<='1';te:=i;i<=te+1;
+		--when 12=>isdone<='0';i<="000000"
+		when 12=>isdone<='0';i<=0;
+		when others =>
+		null;
+		end case;
+		
+	elsif(isstart(0)='1')then--draw功能
+		case i is
+		when 0=>if(spi_done='1')then 
+		isspi_start<='0';te:=i;i<=te+1;
+		else rdata<="00"&"1011"&y;isspi_start<='1';end if;
+		when 4=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"1011"&y;isspi_start<='1';end if;
+		when 8=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"1011"&y;isspi_start<='1';end if;
+		when 12=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"1011"&y;isspi_start<='1';end if;
+		when 16=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"1011"&y;isspi_start<='1';end if;
+		when 20=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"1011"&y;isspi_start<='1';end if;
+		when 24=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"1011"&y;isspi_start<='1';end if;
+		when 28=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"1011"&y;isspi_start<='1';end if;
+		
+		when 1=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0001"&"0000";isspi_start<='1';end if;
+		when 5=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0001"&"0000";isspi_start<='1';end if;
+		when 9=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0001"&"0000";isspi_start<='1';end if;
+		when 13=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0001"&"0000";isspi_start<='1';end if;
+		when 17=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0001"&"0000";isspi_start<='1';end if;
+		when 21=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0001"&"0000";isspi_start<='1';end if;
+		when 25=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0001"&"0000";isspi_start<='1';end if;
+		when 29=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0001"&"0000";isspi_start<='1';end if;
+		
+		when 2=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0000"&"0000";isspi_start<='1';end if;
+		when 6=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0000"&"0000";isspi_start<='1';end if;
+		when 10=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0000"&"0000";isspi_start<='1';end if;
+		when 14=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0000"&"0000";isspi_start<='1';end if;
+		when 18=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0000"&"0000";isspi_start<='1';end if;
+		when 22=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0000"&"0000";isspi_start<='1';end if;
+		when 26=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0000"&"0000";isspi_start<='1';end if;
+		when 30=>if(spi_done='1')then isspi_start<='0';te:=i;i<=te+1;else rdata<="00"&"0000"&"0000";isspi_start<='1';end if;
+		
+		when 3=>if(x="10000000")then tey:=y;y<=tey+1;x<="00000000";te:=i;i<=te+1;elsif(spi_done='1')then isspi_start<='0';tex:=x;x<=tex+1;else rdata<="01"&read_data;isspi_start<='1';end if;
+		when 7=>if(x="10000000")then tey:=y;y<=tey+1;x<="00000000";te:=i;i<=te+1;elsif(spi_done='1')then isspi_start<='0';tex:=x;x<=tex+1;else rdata<="01"&read_data;isspi_start<='1';end if;
+		when 11=>if(x="10000000")then tey:=y;y<=tey+1;x<="00000000";te:=i;i<=te+1;elsif(spi_done='1')then isspi_start<='0';tex:=x;x<=tex+1;else rdata<="01"&read_data;isspi_start<='1';end if;
+		when 15=>if(x="10000000")then tey:=y;y<=tey+1;x<="00000000";te:=i;i<=te+1;elsif(spi_done='1')then isspi_start<='0';tex:=x;x<=tex+1;else rdata<="01"&read_data;isspi_start<='1';end if;
+		when 19=>if(x="10000000")then tey:=y;y<=tey+1;x<="00000000";te:=i;i<=te+1;elsif(spi_done='1')then isspi_start<='0';tex:=x;x<=tex+1;else rdata<="01"&read_data;isspi_start<='1';end if;
+		when 23=>if(x="10000000")then tey:=y;y<=tey+1;x<="00000000";te:=i;i<=te+1;elsif(spi_done='1')then isspi_start<='0';tex:=x;x<=tex+1;else rdata<="01"&read_data;isspi_start<='1';end if;
+		when 27=>if(x="10000000")then tey:=y;y<=tey+1;x<="00000000";te:=i;i<=te+1;elsif(spi_done='1')then isspi_start<='0';tex:=x;x<=tex+1;else rdata<="01"&read_data;isspi_start<='1';end if;
+		when 31=>if(x="10000000")then tey:=y;y<=tey+1;x<="00000000";te:=i;i<=te+1;elsif(spi_done='1')then isspi_start<='0';tex:=x;x<=tex+1;else rdata<="01"&read_data;isspi_start<='1';end if;
+		
+		when 32=>rdata<="1100000000";y<="0000";isdone<='1';te:=i;i<=te+1;
+		--when 33=>isdone<='0';i<="000000";
+		when 33=>isdone<='0';i<=0;
+		when others =>
+		null;
+		end case;
+	end if;
+	end if;
+	end process;
+	
+	
+	temp_add<=x+to_STDLOGICVECTOR((TO_BITVECTOR("00000000"&y) sll 7));--这里的原文是y<<7，依然无法确定位操作的问题
+	read_add<=temp_add(9 downto 0);
+	--read_add<=x+to_STDLOGICVECTOR((conv_integer(y)*128));
+	spi_start<=isspi_start;
+	spi_data<=rdata;
+	--testpin<=temp_add(7);
+	testpin2<=isstart(0);
+	clk_2n<=not isstart(0);
+end architecture;
---
+++ spi.vhd
@@ -0,0 +1,77 @@
+-- spi_write_modeule
+--这个模块接收10位数据，转换为串行输出，其中有一个时间常量确定了半周期分频。
+--遗留问题：除法不能综合成电路，寻找把i进行移位的方案，同时注意原文对i的操作有点奇怪，2/10进制交错使用
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity spi is
+port(
+	clk: in std_logic; 
+	rst:in std_logic;
+	start:in std_logic; 
+	spi_in:in std_logic_vector(9 downto 0);
+	done:out std_logic;
+	spi_out:out std_logic_vector(3 downto 0);clk_3n:out std_logic
+);
+end entity;
+architecture behave of spi is
+	constant f:integer:=9;
+	signal count:integer;
+	signal i:integer;
+	signal rclk,rdo,isdone:std_logic;
+begin
+	process(clk,rst)
+	begin
+	if(clk'event and clk='1')then
+		if(rst='0')then
+		count<=0;
+		elsif(count=f)then
+		count<=0;
+		elsif(start='1')then
+		count<=count+1;
+		else
+		count<=0;
+		end if;
+		end if;
+	end process;
+	
+	process(clk,rst)
+	variable temp:integer;
+	begin
+	if(clk'event and clk='1')then
+	if (rst='0')then
+	i<=0;rclk<='1';rdo<='0';isdone<='0';
+	elsif(start='1')then
+	case i is 
+		when 0 =>if(count=f)then rclk<='0';rdo<=spi_in(7);temp:=i;i<=temp+1;end if;--反正无伤大雅，干脆手写变量，忽略未知部分。最专业的做法是采用类型转换，先暂时这么用，不行再用专业的办法，因为这种方法可能不能总合成电路
+		when 2 =>if(count=f)then rclk<='0';rdo<=spi_in(6);temp:=i;i<=temp+1;end if;
+		when 4 =>if(count=f)then rclk<='0';rdo<=spi_in(5);temp:=i;i<=temp+1;end if;
+		when 6 =>if(count=f)then rclk<='0';rdo<=spi_in(4);temp:=i;i<=temp+1;end if;
+		when 8 =>if(count=f)then rclk<='0';rdo<=spi_in(3);temp:=i;i<=temp+1;end if;
+		when 10 =>if(count=f)then rclk<='0';rdo<=spi_in(2);temp:=i;i<=temp+1;end if;
+		when 12 =>if(count=f)then rclk<='0';rdo<=spi_in(1);temp:=i;i<=temp+1;end if;
+		when 14 =>if(count=f)then rclk<='0';rdo<=spi_in(0);temp:=i;i<=temp+1;end if;
+		when 1=>if(count=f)then rclk<='1';temp:=i;i<=temp+1;end if;
+		when 3=>if(count=f)then rclk<='1';temp:=i;i<=temp+1;end if;
+		when 5=>if(count=f)then rclk<='1';temp:=i;i<=temp+1;end if;
+		when 7=>if(count=f)then rclk<='1';temp:=i;i<=temp+1;end if;
+		when 9=>if(count=f)then rclk<='1';temp:=i;i<=temp+1;end if;
+		when 11=>if(count=f)then rclk<='1';temp:=i;i<=temp+1;end if;
+		when 13=>if(count=f)then rclk<='1';temp:=i;i<=temp+1;end if;
+		when 15=>if(count=f)then rclk<='1';temp:=i;i<=temp+1;end if;
+		when 16=>isdone<='1';temp:=i;i<=temp+1;
+		when 17=>isdone<='0';i<=0;
+		when others =>null;
+		end case;
+	end if;
+	end if;
+		end process;
+		done<=isdone;
+		spi_out(3)<=spi_in(9);
+		spi_out(2)<=spi_in(8);
+		spi_out(1)<=rclk;
+		spi_out(0)<=rdo;
+		clk_3n<=rclk;
+		--spi_out<={spi_in(9),spi_in(8),rclk,rdo};
+end architecture;
